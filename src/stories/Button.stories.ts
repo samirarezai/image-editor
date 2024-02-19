9171115382fdeb9c0227c26310929531708d9720
z.rezayi <samirarezai1996@gmail.com>
2024-02-19 12:29:41 +0330

    remove weight types

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -29,22 +29,22 @@ export const Primary: Story = {
   },
 };
 
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+// export const Secondary: Story = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
+//
+// export const Large: Story = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
+//
+// export const Small: Story = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };

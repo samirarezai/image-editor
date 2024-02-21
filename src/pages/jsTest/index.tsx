fc263c0ac9fb4565889860980815f00849c900b8
z.rezayi <samirarezai1996@gmail.com>
2024-02-21 11:50:23 +0330

    practice

--- src/pages/jsTest/index.tsx
+++ src/pages/jsTest/index.tsx
@@ -1,18 +1,35 @@
 const Index = () => {
-    const value:string = "This is an example!";
-    const jj = ()=>{
-        let final= "";
-        for(let i = value.length-1; i>-1 ; i--){
-            final+=value[i]
+    const employees = [
+        {employeeId: 11, name: 'John', managerId: 11},
+        {employeeId: 50, name: 'Todd', managerId: 73},
+        {employeeId: 150, name: 'Alex', managerId: 200},
+        {employeeId: 73, name: 'Sara', managerId: 11},
+        {employeeId: 200, name: 'Alex', managerId: 50},
+        {employeeId: 100, name: 'Alex', managerId: 150},
+    ];
+    const result:number[] = [];
+    const check = (employees: { employeeId: number; name: string; managerId: number }[], num: number): number[] => {
+        const employee = employees.find((item) => item.employeeId === num);
+        if (employee) {
+            if (employee.employeeId !== employee.managerId) {
+                result.push(employee.managerId);
+                check(employees, employee.managerId)
+            }
         }
-        return final
     }
-console.log(jj())
+    check(employees, 200)
+    console.log(result)
+
     return (
         <div>
-<p>
-    check value:
-</p>
+            <p>
+                check value:
+            </p>
+            <div>
+                <div id="display">0</div>
+                <button id="start">Start</button>
+                <button id="stop">Stop</button>
+            </div>
         </div>
     );
 };

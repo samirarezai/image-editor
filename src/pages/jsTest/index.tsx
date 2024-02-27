ecf0e99eeac662d7c7a17cc3d77b5f6b3a72f498
z.rezayi <samirarezai1996@gmail.com>
2024-02-27 16:04:36 +0330

    remove

--- src/pages/jsTest/index.tsx
+++ src/pages/jsTest/index.tsx
@@ -1,22 +1,5 @@
 const Index = () => {
 
-    function jumpingOnClouds(c: number[]): number {
-        // Write your code here
-        const sunC = c.length - 1;
-        let steps = 0;
-        for (let i = 0; i < sunC; i++) {
-            if (c[i + 2] < 1) {
-                steps += 1;
-                i++;
-            } else {
-                steps += 1;
-            }
-        }
-        return steps;
-    }
-
-    let result = jumpingOnClouds([0, 0, 0 ,0 ,1 ,0]);
-    console.log('result', result);
 
     return (
         <div>

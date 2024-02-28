19f7c43e73f3acbc1dd4a8a66c2ddc3c7774193d
z.rezayi <samirarezai1996@gmail.com>
2024-02-28 11:45:11 +0330

    hashtable challenge

--- src/pages/jsTest/index.tsx
+++ src/pages/jsTest/index.tsx
@@ -1,6 +1,25 @@
 const Index = () => {
 
+    function whatFlavors(cost: number[], money: number): void {
+        // Write your code here
+        const n = cost.length;
+        const listMap = new Map(cost.map((obj, index) => [obj, index]));
 
+        let result = 0;
+        let num = 0;
+        for (let i = 0; i < n; i++) {
+            if (cost[i] < money && !result) {
+                num = i + 1;
+                const value = listMap.get(money - cost[i]);
+                if (value) {
+                    result = value + 1;
+                }
+            }
+        }
+        console.log(`${num} ${result}`)
+    }
+
+    whatFlavors([2, 2, 4, 3], 4)
     return (
         <div>
             <p>

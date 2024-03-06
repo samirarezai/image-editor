885f28447e91b79f473caec0c4912d82c447c29c
z.rezayi <samirarezai1996@gmail.com>
2024-03-06 16:31:47 +0330

    greedy algorithm challenge

--- src/pages/jsTest/index.tsx
+++ src/pages/jsTest/index.tsx
@@ -1,6 +1,29 @@
 const Index = () => {
 
+    function minimumAbsoluteDifference(arr: number[]): number {
+        // Write your code here
+        const ln = arr.length;
 
+        for (let i = 0; i < ln; i++) {
+            for (let j = i; j < ln; j++) {
+                if (arr[j] < arr[i]) {
+                    const cup = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = cup;
+                }
+            }
+        }
+        let result = null;
+        for (let i = 0; i < ln; i++) {
+            const mn = Math.abs(arr[i] - arr[i + 1])
+            if (result === null || mn < result)
+                result = mn
+        }
+
+        return result;
+    }
+
+    console.log(minimumAbsoluteDifference([3, -7, 0]))
     return (
         <div>
             <p>
